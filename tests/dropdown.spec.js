98b22ffe96c921008cf7206e67138178bf80de95
Gopal K <gopal@gigipo.com>
2025-11-01 15:43:28 +0530

    dropdown-new practice

--- tests/dropdown.spec.js
+++ tests/dropdown.spec.js
@@ -2,13 +2,16 @@ import {test, expect } from "@playwright/test";
 
 test('Dropdown handling in Playwright', async({page}) =>{
 
-    await page.goto('https://the-internet.herokuapp.com/dropdown');
-    const dropdown = page.locator('#dropdown');
-    await dropdown.selectOption('1'); //select by value
-    await expect(dropdown).toHaveValue('1');
-    console.log('Selected option with value 1')
-    await dropdown.selectOption({label: 'Option 2'});
-    await expect(dropdown).toHaveValue('2');
-    console.log('Selected option with label Option 2')
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const username = page.locator('input#username');
+    const SignIn = page.locator ('#signInBtn');
+    const dropdown = page.locator('Select.form-control');
+    await dropdown.selectOption('consult'); //select by value
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+
+
+    // assertion
+    await page.pause();
     
 });
